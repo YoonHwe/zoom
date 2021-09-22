2d14f73c08d0aa249d4dd272994d6465ccdff951
YoonHwe <dbsghl676@gmail.com>
2021-09-23 02:51:41 +0900

    #1.4

--- src/server.js
+++ src/server.js
@@ -1,12 +1,32 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
+
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req,res) => res.render("home"));
+app.get("/", (_,res) => res.render("home"));
+app.get("/*", (_,res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000);
+//아래를 통해 http서버와 webSocket서버 둘 다 돌릴 수 있음
+const server = http.createServer(app);//createServer를 하려면 requestListener 경로가 있어야 함 -> app. (express application으로부터 서버를 만드는 과정)
+
+const wss = new WebSocket.Server({ server });//webSocketServer를 만들었음 http서버 위에
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => {
+        console.log("Disconnected from the Browser");
+    })
+    socket.on("message", (message) => {
+        console.log(message.toString('utf-8'));//console.log(message);의 new 'ws' version
+    });
+    socket.send("hello!!");
+});
+
+server.listen(3000, handleListen);
